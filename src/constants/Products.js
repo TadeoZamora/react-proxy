db5999aadb899f92865d7b88e1bb534504f42249
TadeoZamora <zamora.figueroa@gmail.com>
2020-11-11 16:01:42 -0700

    creamos un componente y reenderizamos una lista -basico.

---
+++ src/constants/Products.js
@@ -0,0 +1,29 @@
+//array of products
+export const products = [
+    //object
+    {
+        id : 1,
+        name : 'Apple',
+        price : 5
+    },
+    {
+        id : 2,
+        name : 'Grapes',
+        price : 6
+    },
+    {
+        id : 3,
+        name : 'orange',
+        price : 7
+    },
+    {
+        id : 4,
+        name : 'Melon',
+        price : 8
+    },
+    {
+        id : 5,
+        name : 'watermelon',
+        price : 9
+    },
+]
